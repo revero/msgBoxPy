7ba7ad11009347f0bb5efde20a3968e098fc6f14
Mark Lotspaih <lotspaih@users.noreply.github.com>
2016-11-14 07:19:11 -0500

    Update msgBoxPy.py
    Added simple docstrings

--- msgBoxPy.py
+++ msgBoxPy.py
@@ -15,21 +15,25 @@ import tkinter.messagebox as box
 
 
 def infobox(title='Title', message='Empty'):
+    '''Show information messagebox GUI'''
     Tk().withdraw()
     box.showinfo(title, message)
 
 
 def warningbox(title='Title', message='Empty'):
+    '''Show warning messagebox GUI'''
     Tk().withdraw()
     box.showwarning(title, message)
 
 
 def errorbox(title='Title', message='Empty'):
+    '''Show error messagebox GUI'''
     Tk().withdraw()
     box.showerror(title, message)
 
 
 def yesnobox(title='Title', message='Empty'):
+    '''Show Yes/No messagebox GUI and return answer'''
     Tk().withdraw()
     answer = box.askquestion(title, message)
     return answer  # Returns 'yes' or 'no'
