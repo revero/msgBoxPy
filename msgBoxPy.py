1edf8e34fafd2e5bb2b89d2db6ef9fe1141429d3
Mark Lotspaih <lotspaih@users.noreply.github.com>
2017-01-04 10:43:09 -0500

    Add files via upload

--- msgBoxPy.py
+++ msgBoxPy.py
@@ -5,13 +5,15 @@
 # Copyright (c) 2016 Mark Lotspaih
 # MIT License - https://opensource.org/licenses/MIT
 
-# msgBoxPy - Quick import or reference for making simple GUI message boxes.
-# USAGE: "import msgBoxPy" or copy the examples below. Two variables, "title"
-#   and "message" must be submitted to the function or replaced if the example
-#   is copied directly into a script.
+# msgBoxPy - Quick import or reference for making simple GUI messages, input,
+#    and dialog boxes.
+# USAGE: "import msgBoxPy" or copy the examples (including imports) below.
+# VERSION: 01/04/2017
 
 from tkinter import Tk
 import tkinter.messagebox as box
+from tkinter import filedialog
+from tkinter import simpledialog
 
 
 def infobox(title='Title', message='Empty'):
@@ -36,7 +38,45 @@ def errorbox(title='Title', message='Empty'):
 
 
 def yesnobox(title='Title', message='Empty'):
-    '''Show Yes/No messagebox GUI and return answer'''
+    '''Show Yes/No messagebox GUI and return yes or no answer'''
     Tk().withdraw()
     answer = box.askquestion(title, message)
-    return answer  # Returns 'yes' or 'no'
+    return answer
+
+
+# TODO: OK/Cancel box
+
+
+# TODO: Retry/Cancel box
+
+
+def inputbox(title='Title', prompt='Prompt text:'):
+    '''Creates a simple string input box with Ok/Cancel buttons. Ok returns
+    the string entered unless the string is empty then "empty" is returned.
+    Cancel returns "cancel".'''
+    Tk().withdraw()
+    enteredString = simpledialog.askstring(title, prompt)
+    if enteredString == None:
+        return 'cancel'
+    elif enteredString == '':
+        return 'empty'
+    else:
+        return enteredString
+
+
+# TODO: Integer input box
+
+
+def selectfilenamebox(**kwargs):
+    '''Creates a select file dialog box and returns path to the file
+    selected or returns empty if Cancel was selected.
+    Example keyword options are "title" and "initialdir".'''
+    Tk().withdraw()
+    filePath = filedialog.askopenfilename(**kwargs)
+    return filePath
+
+
+# TODO: Select file save box
+
+
+# TODO: Select folder box
